f7f61e2ab04a6947f9078533329d372192f80282
Farhan Sadik <farhansadik0760@gmail.com>
2025-11-06 09:48:22 +0600

    socket io remove from backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,9 @@
-import http from "http";
 import app from "./src/app.js";
 import connectDB from "./src/db/db.js";
-import initialSocketServer from "./src/sockets/socket.server.js";
 
-connectDB(); // MongoDB connect
-
-const server = http.createServer(app); // Express + HTTP server
-
-initialSocketServer(server); // Socket.io attach
+connectDB();
 
 const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
