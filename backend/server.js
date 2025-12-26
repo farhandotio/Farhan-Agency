f32bf5b7d1a965fd3d0dee5a2e2729255530eeef
farhandotio <farhansadik0760@gmail.com>
2025-12-26 09:03:42 +0600

    backend port update

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import connectDB from "./src/db/db.js";
 
 connectDB();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
